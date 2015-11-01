7638864240cac4980385acf89f7f25fe097ccb86
etkirsch <etkirsch@gmail.com>
2015-11-01 06:26:58 -0800

    NPCs now have stat blocks; each class has stat preferences

--- objects/npc.py
+++ objects/npc.py
@@ -1,25 +1,41 @@
 from objects.randomobject import RandomObject
 from objects.spellbookgenerator import SpellBookGenerator
 from random import shuffle
-import random
+import random, math
 
 class Npc(RandomObject):
     parameter_types = []
 
     def __init__(self):
         super().__init__("data/unlocked/character.json")
-        self.strength = 10
-        self.dexterity = 10
-        self.constitution = 10
-        self.intelligence = 10
-        self.wisdom = 10
-        self.charisma = 10
-        self.level = random.randint(0, 20) + 1
+        self.randomizeStats()
+        self.level = int(random.betavariate(2,5)*19)+1
         self.spell_book = ""
         self.sbg = SpellBookGenerator()
 
+    def randomize(self, type_to_randomize):
+        if type_to_randomize == 'playable_class':
+            type_values = [x for x in self.parameter_types[type_to_randomize]]
+            random_val = random.randint(0,len(type_values)-1)
+            self.playable_class = type_values[random_val]
+            self.stat_preferences = self.parameter_types['playable_class'][self.playable_class]['stat_preference']
+            self.hitDie = self.parameter_types['playable_class'][self.playable_class]['hitDie']
+            return
+        super().randomize(type_to_randomize)
+
+    # SLOPPY BUT: It works for now
+    def randomizeStats(self):
+        val = [0,0,0,0,0,0]
+        for x in range(0,6):
+            val[x] = 8 + int(random.betavariate(2,4)*12)
+        stats = sorted(val, reverse=True)
+        x = 0
+        for stat in self.stat_preferences:
+            setattr(self,stat,stats[x])
+            x += 1
+
     def describe(self,from_perspective=None):
-        description = '\n'
+        description = '\nSTR:\t{0}\tDEX:\t{1}\tCON:\t{2}\nINT:\t{3}\tWIS:\t{4}\tCHA:\t{5}\n'.format(self.strength,self.dexterity,self.constitution,self.intelligence,self.wisdom,self.charisma)
         if self.sbg.hasSpells(self.playable_class):
             if self.spell_book is "":
                 self.spell_book = self.makeSpellBook()
